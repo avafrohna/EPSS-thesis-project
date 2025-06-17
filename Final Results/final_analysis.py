d182e2554050fc71a1413f755ed4abdadf8345b5
Ava Frohna <122284870+avafrohna@users.noreply.github.com>
2025-06-17 00:42:46 -0700

    final data analysis

--- Final Results/final_analysis.py
+++ Final Results/final_analysis.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 
-csv_path = "../final_plots_sbert_0_alpha/only_cvss_leave_out_cve/actual_vs_pred_20250615_172828.csv"
+csv_path = "../final_plots_sbert_0_alpha/only_cve/actual_vs_pred_20250615_174131.csv"
 
 df = pd.read_csv(csv_path)
 df['absolute_difference'] = (df['predicted'] - df['actual']).abs()
